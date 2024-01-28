c40375a776c7e13be6e7c977ab8049d210734942
denielfer <dfc152@gmail.com>
2024-01-28 18:23:59 -0300

    ajustes monolito

--- django-monolito/UnMEP/tarefas_crud/views.py
+++ django-monolito/UnMEP/tarefas_crud/views.py
@@ -8,23 +8,25 @@ def home(request):
 
 def tarefas(request):
     form = Tarefa_form()
-    if request.method == 'POST':
-        if "save" in request.POST:
-            id = request.POST.get('save')
-            print(id)
-            if not id:
-                form = Tarefa_form(request.POST)
-            else:
-                print(request.POST)
-                tarefa_edit = Tarefa.objects.get( id = id )
-                form = Tarefa_form(request.POST,instance = tarefa_edit)
-            form.save()
-            form = Tarefa_form()
-        elif 'deletar' in request.POST:
-            Tarefa.objects.get( id = request.POST.get('deletar') ).delete()
-        elif 'edit' in request.POST:
-            tarefa_edit = Tarefa.objects.get( id = request.POST.get('edit') )
-            form = Tarefa_form(instance = tarefa_edit)
+    try:
+        if request.method == 'POST':
+            if "save" in request.POST:
+                id = request.POST.get('save')
+                if id is None or id == 'None':
+                    form = Tarefa_form(request.POST)
+                else:
+                    print(request.POST)
+                    tarefa_edit = Tarefa.objects.get( id = id )
+                    form = Tarefa_form(request.POST,instance = tarefa_edit)
+                form.save()
+                form = Tarefa_form()
+            elif 'deletar' in request.POST:
+                Tarefa.objects.get( id = request.POST.get('deletar') ).delete()
+            elif 'edit' in request.POST:
+                tarefa_edit = Tarefa.objects.get( id = request.POST.get('edit') )
+                form = Tarefa_form(instance = tarefa_edit)
+    except Exception as e:
+        print('Error:', str(e))
     context = {
         "tarefas": Tarefa.objects.all(),
         "form": form
